91f92d938d1f1e0c165e99b1657e688dc0163b3a
fahriazdev <fakhriazdev@gmail.com>
2024-08-07 20:23:03 +0700

    [FIX:] type response comment

--- app/lib/features/commentsSlice.ts
+++ app/lib/features/commentsSlice.ts
@@ -1,57 +1,60 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import toast from 'react-hot-toast';
-import CommentService from "@/app/lib/services/commentService";
-
+import CommentService from '@/app/lib/services/commentService';
+interface Comment {
+  uuid: string;
+  projectId: string;
+  userId: string;
+  createdAt: string;
+  comment: string;
+}
 
 interface CommentsState {
-    comments: any[];
+  comments: Comment[];
 }
 
 const initialState: CommentsState = {
-    comments: [],
+  comments: [],
 };
 
-
-const commentsAction = createAsyncThunk<Comment[], string, { rejectValue: string }>(
-    'comments/fetchComments',
-    async (postId: string, { rejectWithValue }) => {
-        try {
-            const response = await CommentService.getComment(postId);
-            return response.data;
-        } catch (error: any) {
-            const errorMessage = error.response?.data?.message || 'An error occurred';
-            return rejectWithValue(errorMessage);
-        }
-    }
-);
-
+const commentsAction = createAsyncThunk<
+  Comment[],
+  string,
+  { rejectValue: string }
+>('comments/fetchComments', async (postId: string, { rejectWithValue }) => {
+  try {
+    const response = await CommentService.getComment(postId);
+    return response.data;
+  } catch (error: any) {
+    const errorMessage = error.response?.data?.message || 'An error occurred';
+    return rejectWithValue(errorMessage);
+  }
+});
 
 const commentsSlice = createSlice({
-    name: 'comments',
-    initialState,
-    reducers: {
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(commentsAction.pending, () => {
-                toast.loading("Loading...");
-            })
-            .addCase(commentsAction.fulfilled, (state, { payload}) => {
-
-
-                const newComments = payload.filter(
-                    (newComment) => !state.comments.some(comment => comment.uuid === newComment.uuid)
-                );
-                state.comments = [...state.comments, ...newComments];
-                console.log(state.comments)
-                toast.dismiss();
-            })
-            .addCase(commentsAction.rejected, (_, action) => {
-                const errorMessage = action.payload as string;
-                toast.dismiss();
-                toast.error(errorMessage);
-            });
-    },
+  name: 'comments',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(commentsAction.pending, () => {
+        toast.loading('Loading...');
+      })
+      .addCase(commentsAction.fulfilled, (state, { payload }) => {
+        const newComments = payload.filter(
+          (newComment) =>
+            !state.comments.some((comment) => comment.uuid === newComment.uuid)
+        );
+        state.comments = [...state.comments, ...newComments];
+        console.log(state.comments);
+        toast.dismiss();
+      })
+      .addCase(commentsAction.rejected, (_, action) => {
+        const errorMessage = action.payload as string;
+        toast.dismiss();
+        toast.error(errorMessage);
+      });
+  },
 });
 
 // export const { toggleVisibility } = commentsSlice.actions;
