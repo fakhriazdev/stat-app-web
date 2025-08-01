15bafd5b60df0518be13d3fce78f137c8ec08a90
fakhriazdev <fakhriazdev@gmail.com>
2025-08-02 04:56:18 +0700

    [CHANGE]: change destination end point backend

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://nest-api-portfolio.onrender.com/api/:path*',
+        destination: 'https://nest-api-portfolio.vercel.app/api/:path*',
       },
     ];
   },
